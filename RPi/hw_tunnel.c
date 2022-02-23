e7a198a93eb3e1aa8fecb5bdc443dba9606fd593
Varban Metodiev <varban.metodiev@gmail.com>
2022-02-23 18:45:57 +0200

    current progress, IPv6 solicitation packet received over UART

--- RPi/hw_tunnel.c
+++ RPi/hw_tunnel.c
@@ -1,6 +1,17 @@
-/*
-    Tun/Tap Interface Creation Example
-*/
+//
+// Compile with: gcc -Wall -o hw_tunnel hw_tunnel.c -lwiringPi -lpthread
+//
+
+//
+//
+// For best performance, make sure you have the following "isolcpus" lines inside the cmdline file
+//
+// $ cat /boot/cmdline.txt
+// console=serial0,115200 console=tty1 root=PARTUUID=ba069350-02 rootfstype=ext4 elevator=deadline fsck.repair=yes rootwait isolcpus=2,3
+//
+//
+
+#define _GNU_SOURCE
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -13,15 +24,34 @@
 #include <linux/if.h>
 #include <linux/if_tun.h>
 #include <signal.h>
+#include <errno.h>
+#include <poll.h>
+#include <time.h>
+#include <sched.h>
+#include <pthread.h>
 
-#define TUN_TAP_IFACE_NAME "inversg"
+#include <wiringPi.h>
+#include <wiringSerial.h>
+
+#define TUN_TAP_IFACE_NAME "inverseg0"
+#define COM_PORT_A_NAME "/dev/ttyUSB0"
+#define COM_PORT_RATE 115200
+#define TUNNEL_PAIR_Q_SIZE_IN_BYTES 4096
 
 // Globals
-int tuntapfd = -1;
+typedef struct tunnel_pair{
+    int tuntapfd;
+    int serialfd;
+} tunnel_pair;
+
+tunnel_pair tun_pair;
 
 // Function prototypes
 int tun_tap_iface_create(char *name, int type);
+int serial_port_create(char *name, int baud);
 void signal_handler(int signal);
+void *read_tuntap_write_serial(void *param);
+void *read_serial_write_tuntap(void *param);
 
 // Functions implementations
 int tun_tap_iface_create(char *name, int type)
@@ -51,14 +81,82 @@ int tun_tap_iface_create(char *name, int type)
     return fd;
 }
 
+int serial_port_create(char *name, int baud)
+{
+    int fd;
+
+    // Initialise wiringPi setup
+    if (wiringPiSetup () == -1)                                        
+    {
+        fprintf ( stderr, "Unable to start wiringPi: %s\n", strerror (errno) ) ;
+        return 1 ;
+    }
+
+    // Open serial port A
+    if ( (fd = serialOpen (name, baud)) < 0 )
+    {
+        fprintf ( stderr, "Unable to open serial device: %s\n", strerror (errno) ) ;
+        close(fd);
+    }
+
+    return fd;
+}
+
 void signal_handler(int signal)
 {
     printf("\nTerminting...\n");
-    close(tuntapfd);
+
+    printf("\nClosing the tunnel interface...");
+    close(tun_pair.tuntapfd);
+
+    printf("\nClosing the serial port...");
+    close(tun_pair.serialfd);
+
+    printf("\nDone!\n");
     exit(1);
 }
 
-void main(void)
+void *read_tuntap_write_serial_thread(void *param)
+{
+    tunnel_pair* pair = (tunnel_pair *)param;
+    int tuntapfd = pair->tuntapfd;
+    int serialfd = pair->serialfd;
+
+    char buffer[TUNNEL_PAIR_Q_SIZE_IN_BYTES] = { 0 };
+    int len = 0;
+
+    while(1)
+    {
+        len = read(tuntapfd, buffer, sizeof(buffer));
+        if (len > 0)
+        {
+            // TBD: Use 'write' in a loop to guarantee that all bytes have been sent
+            write(serialfd, buffer, len);
+        }
+    }
+}
+
+void *read_serial_write_tuntap_thread(void *param)
+{
+    tunnel_pair* pair = (tunnel_pair *)param;
+    int tuntapfd = pair->tuntapfd;
+    int serialfd = pair->serialfd;
+
+    char buffer[TUNNEL_PAIR_Q_SIZE_IN_BYTES] = { 0 };
+    int len = 0;
+
+    while(1)
+    {
+        len = read(serialfd, buffer, sizeof(buffer));
+        if (len > 0)
+        {
+            // TBD: Use 'write' in a loop to guarantee that all bytes have been sent
+            write(tuntapfd, buffer, len);
+        }
+    }
+}
+
+int main(void)
 {
     signal(SIGHUP,  signal_handler);
     signal(SIGTERM, signal_handler);
@@ -66,20 +164,58 @@ void main(void)
     signal(SIGSTOP, signal_handler);
     signal(SIGINT,  signal_handler);
 
-    tuntapfd = tun_tap_iface_create(TUN_TAP_IFACE_NAME, IFF_TUN);
-    char buffer[256] = { 0 };
-    int len = 0;
+    // Attempt to create the tun/tap
+    tun_pair.tuntapfd = tun_tap_iface_create(TUN_TAP_IFACE_NAME, IFF_TUN);
 
-    if (tuntapfd > 0)
+    if (tun_pair.tuntapfd > 0)
         printf("Interface %s successfully created!\n", TUN_TAP_IFACE_NAME);
     else
-        printf("Error creating the %s interface: fd is %d\n", TUN_TAP_IFACE_NAME, tuntapfd);
+        printf("Error creating the %s interface: fd is %d\n", TUN_TAP_IFACE_NAME, tun_pair.tuntapfd);
+
+    // Attempt to create the serial port
+    tun_pair.serialfd = serial_port_create(COM_PORT_A_NAME, COM_PORT_RATE);
+
+    if (tun_pair.serialfd > 0)
+        printf("Serial port %s successfully created!\n", COM_PORT_A_NAME);
+    else
+        printf("Error creating the %s serial port: fd is %d\n", COM_PORT_A_NAME, tun_pair.serialfd);
+
+
+    cpu_set_t cpuset;
+
+    pthread_t thread1;
+    pthread_t thread2;
+
+    pthread_create( &thread1, 0, read_tuntap_write_serial_thread,  (void *)&tun_pair );
+    pthread_create( &thread2, 0, read_serial_write_tuntap_thread,  (void *)&tun_pair );
+
+    int s;
+    CPU_SET(2, &cpuset);
+    s = pthread_setaffinity_np(thread1, sizeof(cpuset), &cpuset);
+    if (s != 0)
+        fprintf( stderr, "FATAL: Could not set CPU core affinity for the read_tuntap_write_serial_thread..." );
+
+    CPU_SET(3, &cpuset);
+    s = pthread_setaffinity_np(thread2, sizeof(cpuset), &cpuset);
+    if (s != 0)
+        fprintf( stderr, "FATAL: Could not set CPU core affinity for the read_serial_write_tuntap_thread..." );
+
+    pthread_join( thread1, NULL );
+    pthread_join( thread2, NULL );
+
+    /*
+    //int rx_bytes = 0;
+    //int tx_bytes = 0;
 
     while(1)
     {
-        len = read(tuntapfd, buffer, sizeof(buffer));
-        printf("\nReceived %d bytes...", len);
+        len = read(tun_pair.tuntapfd, buffer, sizeof(buffer));
+        if (len > 0)
+        {
+            write(tun_pair.serialfd, buffer, len);
+        }
     }
+    */
 
-    close(tuntapfd);
+    return 0;
 }
